a2116c843478bc5b1639e0da2507670466b7d8bd
Tunglone <62212213+Tunglone@users.noreply.github.com>
2020-07-23 11:25:10 +0700

    Create Bai6.js

---
+++ Bai6.js
@@ -0,0 +1,12 @@
+/*  Sự khác nhau giữa mảng và đối tượng:
+- Mảng có độ dài còn Obj thì không, ngoài ra mảng còn có rất nhiều method như map, reduce, ...
+- Trong mảng, mỗi phần tử ứng với 1 số thứ tự và mỗi lần gọi phần tử đó ra chỉ cần gọi tenMang[soThuTu] VD:
+    var arr = ['A', 'B', 'C']
+    arr[0];;
+  Trong object, mỗi 1 phần tử ứng với 1 key, mỗi lần gọi đến phần tử sử dụng cú pháp tenOject.tenKey VD:
+    var obj = {
+      name: 'Tung',
+      age: 21
+    }
+    obj.age;
+ */
