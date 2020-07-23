a0b4cadb4d7cfbe2e8029ad964ca8ac731953596
Tunglone <62212213+Tunglone@users.noreply.github.com>
2020-07-23 12:10:10 +0700

    Create Bai12_1.js

---
+++ Bai12_1.js
@@ -0,0 +1,7 @@
+var a = [1, 2];
+var b = [1, 2];
+console.log(a === b);
+/*
+  * a chỉ bằng b khi cho a = b
+  * Việc gán a = [1, 2] và b = [1, 2] là ta đang gán a, b đến 2 đối tượng khác nhau và được đặt ở 2 vùng nhớ khác nhau
+*/
